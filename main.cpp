c3bc2f46149b6c6cf6d1a96a602bc54de81bfd00
Sven-Hendrik Haase <sh@lutzhaase.com>
2010-08-26 01:56:50 +0200

    Completion.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <vector>
+
 int main() {
 	boost::posix_time::ptime current_date = boost::posix_time::microsec_clock::local_time();
 	boost::posix_time::time_duration current_time = current_date.time_of_day();
@@ -20,23 +21,35 @@ int main() {
 	break_times.push_back(boost::posix_time::time_duration(10,20,0));
 	break_times.push_back(boost::posix_time::time_duration(10,25,0));
 	break_times.push_back(boost::posix_time::time_duration(11,10,0));
-	break_times.push_back(boost::posix_time::time_duration(11,15,0));
+	break_times.push_back(boost::posix_time::time_duration(11,25,0));
 	break_times.push_back(boost::posix_time::time_duration(12,10,0));
+	break_times.push_back(boost::posix_time::time_duration(12,15,0));
 	break_times.push_back(boost::posix_time::time_duration(13,00,0));
 	break_times.push_back(boost::posix_time::time_duration(13,20,0));
 	break_times.push_back(boost::posix_time::time_duration(14,05,0));
 	break_times.push_back(boost::posix_time::time_duration(14,10,0));
 	break_times.push_back(boost::posix_time::time_duration(14,55,0));
+	break_times.push_back(boost::posix_time::time_duration(01,55,0));
 
-	sf::RenderWindow RenderWin(sf::VideoMode(800, 600, 32), "MegaGong");
+	float time_since_last_play = 100.f;
+
+	sf::RenderWindow RenderWin(sf::VideoMode::GetDesktopMode(), "MegaGong", sf::Style::Fullscreen);
 
 	sf::Font MonoFont;
-	if(!MonoFont.LoadFromFile("DejaVuSansMono.ttf")) {
+	if(!MonoFont.LoadFromFile("DejaVuSansMono.ttf", 100)) {
 		std::cerr << "Error while loading font, aborting..." << std::endl;
 		return 1;
 	}
+	sf::String TimeText("time", MonoFont, 100);
+	TimeText.SetCenter(TimeText.GetRect().GetWidth() / 2, TimeText.GetRect().GetHeight() / 2);
+	TimeText.SetPosition(RenderWin.GetWidth() / 4, RenderWin.GetHeight() / 2);
 
-	sf::String TimeText("time", MonoFont, 50);
+	sf::SoundBuffer Ring;
+	if(!Ring.LoadFromFile("jetzt_aber_raus_hier.ogg")) {
+		std::cerr << "Error while loading sound, aborting..." << std::endl;
+		return 1;
+	}
+	sf::Sound RingSound(Ring);
 
 	// Main loop.
 	while(RenderWin.IsOpened()) {
@@ -48,20 +61,31 @@ int main() {
 				RenderWin.Close();
 		}
 
-
-		RenderWin.Clear();
+		time_since_last_play += RenderWin.GetFrameTime();
 
 		current_time = boost::posix_time::microsec_clock::local_time().time_of_day();
 		TimeText.SetText(boost::posix_time::to_simple_string(current_time));
+
+		if(time_since_last_play <= 5.f) {
+			TimeText.SetColor(sf::Color(255, time_since_last_play * 50, time_since_last_play * 50));
+			RenderWin.Clear(sf::Color(sf::Randomizer::Random(0, 255), sf::Randomizer::Random(0, 255), sf::Randomizer::Random(0, 255)));
+		}
+		else {
+			TimeText.SetColor(sf::Color(255, 255, 255));
+			RenderWin.Clear();
+		}
+
+		BOOST_FOREACH(auto break_time, break_times) {
+			if (break_time.hours() == current_time.hours() && break_time.minutes() == current_time.minutes() && time_since_last_play > 60.f) {
+				RingSound.Play();
+				std::cout << "GONG! at" << current_time << std::endl;
+				TimeText.SetColor(sf::Color::Red);
+				time_since_last_play = 0.f;
+			}
+		}
 		
 		RenderWin.Draw(TimeText);
 
 		RenderWin.Display();
-	}
-
-	std::cout << current_time << std::endl;
-	BOOST_FOREACH(auto break_time, break_times) {
-		if (break_time.hours() == current_time.hours() && break_time.minutes() == current_time.minutes())
-			std::cout << "lol" << std::endl;
 	}
 }
